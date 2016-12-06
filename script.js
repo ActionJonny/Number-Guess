38fb91b4099992d0ebbf2f9d80d9a3e22d6c2148
Jon Willemain <jon.willemain@gmail.com>
2016-12-06 10:11:43 -0700

    input not disabled when user number is outside of number range

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 var guess = document.querySelector('.guess')
+var clear = document.querySelector('.clear')
+var reset = document.querySelector('.reset')
+var minNum = 1
+var maxNum = 200
+var number = Math.ceil(Math.random() * maxNum * minNum)
+var userPlaceHolder= document.querySelector('.user-guess').placeholder='Enter your guess'
+console.log(number)
+
+if(userPlaceHolder < minNum) {
+  document.querySelector('.user-guess').disabled = true
+} if (userPlaceHolder > maxNum) {
+  document.querySelector('.user-guess').disabled = true
+}
 
 guess.addEventListener('click', function() {
   var userGuess = document.querySelector('.user-guess')
   var display = document.querySelector('.div')
-  display.innerText = userGuess.value
+  var parse = parseInt(userGuess.value)
+  display.innerText = userGuess.value;
+
+  if(parse === number) {
+    display.innerText = ('Your last guess was' + userGuess.value + 'Boom!')
+  } else if (parse < number) {
+    display.innerText = ('Your last guess was' + userGuess.value + 'That was too Low!')
+  } else if (parse > number) {
+    display.innerText = ('Your last guess was' + userGuess.value + 'That too High!')
+  } else {
+    display.innerText = 'Please Enter A Number Between ' + minNum + '-' + maxNum
+  }
+})
+
+clear.addEventListener('click', function() {
+  var userGuess = document.querySelector('.user-guess')
+  userGuess.value = ''
+})
+
+reset.addEventListener('click', function() {
+  number = Math.floor(Math.random() * maxNum * minNum)
+  console.log(number)
+  var userGuess = document.querySelector('.user-guess');
+  var display = document.querySelector('.div')
+  userGuess.value = ''
+  display.innerText = ''
 })
